52d24238ffe568f80f88e8b2f52196c2374acc9c
Gustavo Leonardo Simon <gustavosimon.va014@academico.ifsul.edu.br>
2025-04-27 10:53:14 -0300

    Create script.sql

---
+++ senha/script.sql
@@ -0,0 +1,27 @@
+create schema senha;
+
+use senha;
+
+-- Criação da tabela de Usuários
+-- Criação da tabela de Usuários com campo de data
+CREATE TABLE Usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT, -- Use SERIAL para PostgreSQL
+    nomeUsuario VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    dataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Criação da tabela de Histórico de Senhas
+CREATE TABLE HistoricoSenha (
+    idHistorico INT PRIMARY KEY AUTO_INCREMENT, -- Use SERIAL para PostgreSQL
+    idUsuario INT NOT NULL,
+    senhaAntiga VARCHAR(255) NOT NULL,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+-- insert para testar validação de 45 dias da senha
+
+
+INSERT INTO Usuario (nomeUsuario, senha, dataCriacao) VALUES ('Geison', 'senha123', '2025-03-13');
+insert into historicosenha (idUsuario, senhaAntiga) values (1, 'senha123');
